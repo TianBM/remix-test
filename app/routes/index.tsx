334c66978165e3b7ac27573647cb8d10305508dc
田竣友 <junyou_tian@icloud.com>
2021-12-03 11:06:15 +0800

    测试部署

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -61,6 +61,7 @@ export default function Index() {
   return (
     <div className="remix__page">
       <main>
+        <h2>test deploy</h2>
         <h2>Welcome to Remix!</h2>
         <p>We're stoked that you're here. 🥳</p>
         <p>
